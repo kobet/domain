c1ac06bd88ad660c5105f084b66ccb84e4c37432
kobet <weigeshikebi@163.com>
2018-09-01 10:33:17 +0800

    init project

---
+++ achilles/achilles-account/src/main/java/com/domain/achilles/account/LoginController.java
@@ -0,0 +1,13 @@
+package com.domain.achilles.account;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/9/1 0001 10:07
+ * @Description:
+ */
+@RestController
+public class LoginController {
+
+}
---
+++ achilles/achilles-manage/src/main/java/com/domain/achilles/manage/ManageController.java
@@ -0,0 +1,16 @@
+package com.domain.achilles.manage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/9/1 0001 10:30
+ * @Description:
+ */
+@Controller
+@RequestMapping("/manage")
+public class ManageController {
+
+
+}
